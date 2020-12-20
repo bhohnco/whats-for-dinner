5dee3d98000f4a16f963759ddac9a8ec0716b973
bhohnco <bryanhohn13@gmail.com>
2020-12-20 07:30:55 -0700

    Complete comp and added HTML

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
-// var letsCookButton = document.querySelector();
-// var radioButtonOne = document.querySelector();
-// var radioButtonTwo = document.querySelector();
-// var radioButtonThree = document.querySelector();
-// var radioButtonFour = document.querySelector();
-// var addRecpie = document.querySelector();
-//
-// letsCookButton.addEventListener('click')
-// radioButtonOne.addEventListener('click')
+var letsCookButton = document.querySelector('.lets-cook-button');
+var sideButton = document.getElementById('Side');
+var mainButton = document.getElementById('Main Dish');
+var dessertButton = document.querySelector('Dessert');
+var entireMealButton = document.querySelector('Entire Meal');
+var cookAll = document.getElementByName('dishes');
+var
+var addRecpie = document.querySelector();
 
-//
-// var sides =['Miso Glazed Carrots', 'Coleslaw', 'Garden Salad', 'Crispy Potatoes',
-// 'Sweet Potato Tots', 'Coconut Rice', 'Caesar Salad', 'Shrimp Summer Rolls',
-// 'Garlic Butter Mushrooms', 'Hush Puppies']
-// var mains =['Spaghetti and Meatballs', 'Pineapple Chicken', 'Shakshuka', 'Thai Yellow Curry',
-// 'Bibimbap', 'Chicken Parmesean', 'Butternut Squash Soup', 'BBQ Chicken Burgers', 'Ramen',
-// 'Empanadas', 'Chicken Fried Rice', 'Sheet Pan Fajitas', 'Margarita Pizza']
-// var dessert =['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake', 'Banana Bread', 'Peach Cobbler'
-// 'Cheesecake', 'Funfetti Cake', 'Baklava', 'Flan', 'Macaroons', 'Chocolate Cupcakes', 'Pavlova',
-// 'Pumpkin Pie', 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs']
-//
-// function getRandomIndex(array) {
-//   return Math.floor(Math.random() * array.length);
-// };
+var sides =['Miso Glazed Carrots', 'Coleslaw', 'Garden Salad', 'Crispy Potatoes','Sweet Potato Tots', 'Coconut Rice', 'Caesar Salad', 'Shrimp Summer Rolls', 'Garlic Butter Mushrooms', 'Hush Puppies']
+var mains =['Spaghetti and Meatballs', 'Pineapple Chicken', 'Shakshuka', 'Thai Yellow Curry', 'Bibimbap', 'Chicken Parmesean', 'Butternut Squash Soup', 'BBQ Chicken Burgers', 'Ramen', 'Empanadas', 'Chicken Fried Rice', 'Sheet Pan Fajitas', 'Margarita Pizza']
+var dessert =['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake', 'Banana Bread', 'Peach Cobbler','Cheesecake', 'Funfetti Cake', 'Baklava', 'Flan', 'Macaroons', 'Chocolate Cupcakes', 'Pavlova', 'Pumpkin Pie', 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs']
+
+letsCookButton.addEventListener("click", showFoodOptions);
+
+// function showFoodOptions(){
+// var randomSidesIndex = Math.floor(Math.random() * sides.length);
+// var randomMainIndex = Math.floor(Math.random() * mains.length);
+// var randomDessertIndex = Math.floor(Math.random() * dessert.length);
